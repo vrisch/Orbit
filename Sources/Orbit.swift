0986d2b835874a2e38e4daf257d26ffbab68a77d
Vrisch <magnus@vrischikasana.se>
2017-08-30 08:44:47 +0200

    First

--- Sources/Orbit.swift
+++ Sources/Orbit.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+
+public final class Disposable {
+    public static let none = Disposable.init()
+    public static let dispose = Disposable.init(dispose:)
+
+    
+    public init() {
+        self.dispose = nil
+    }
+    
+    public init(dispose: @escaping () -> Void) {
+        self.dispose = dispose
+    }
+
+    deinit {
+        empty()
+    }
+    
+    private let dispose: (() -> Void)?
+}
+
+public extension Disposable {
+    
+    public func empty() {
+        dispose?()
+    }
+}
