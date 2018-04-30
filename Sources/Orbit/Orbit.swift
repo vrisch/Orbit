dfe15d43df0f4a3bb2c844f73de53c7618e8de8b
Magnus Nilsson <magnus.nilsson@smartrefill.se>
2018-04-30 08:32:33 +0200

    Rewrote implementation

--- Sources/Orbit/Orbit.swift
+++ Sources/Orbit/Orbit.swift
@@ -11,53 +11,35 @@ import Foundation
 public final class Disposables {
 
     public init() {
-        self.object = nil
-        self.others = []
-    }
-    
-    public init(object: Any) {
-        self.object = object
-        self.others = []
     }
 
     deinit {
         empty()
     }
 
-    var object: Any?
-    var others: [Disposables]
-    weak var parent: Disposables?
+    var objects: [Any] = []
 }
 
 public extension Disposables {
 
-    public var isEmpty: Bool { return object == nil && others.count == 0 }
-
-    public var count: Int {
-        var result = 0
-        if object != nil { result += 1 }
-        others.forEach { result += $0.count }
-        return result
-    }
+    public var count: Int { return objects.count }
+    public var isEmpty: Bool { return objects.isEmpty }
 
     public func empty() {
-        object = nil
-        others.forEach { $0.empty() }
-        others = []
+        objects.removeAll()
     }
 
     public func add(disposable: Any) {
-        add(disposable: Disposables(object: disposable))
+        objects.append(disposable)
     }
 
     public func add(disposables: [Any]) {
-        disposables.forEach { add(disposable: Disposables(object: $0)) }
+        objects += disposables
     }
 
-    public func add(disposable: Disposables) {
-        guard disposable.parent == nil else { abort() }
-        disposable.parent = self
-        others.append(disposable)
+    public func add(disposables: [Any]?) {
+        guard let disposables = disposables else { return }
+        add(disposables: disposables)
     }
 
     static public func +=(lhs: inout Disposables, rhs: Any) {
@@ -65,6 +47,10 @@ public extension Disposables {
     }
 
     static public func +=(lhs: inout Disposables, rhs: [Any]) {
+        lhs.add(disposables: rhs)
+    }
+
+    static public func +=(lhs: inout Disposables, rhs: [Any]?) {
         lhs.add(disposables: rhs)
     }
 }
