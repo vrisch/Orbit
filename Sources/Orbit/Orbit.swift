376b7fa5af99a34749863d36f3486794914cde89
Vrisch <magnus@vrischikasana.se>
2019-02-13 10:30:27 +0100

    Page support

--- Sources/Orbit/Orbit.swift
+++ Sources/Orbit/Orbit.swift
@@ -50,3 +50,39 @@ extension Index: Hashable {
         hasher.combine(id)
     }
 }
+
+public struct PageIndex: Codable {
+    public let page: Int
+    public let size: Int
+
+    public init(page: Int, size: Int) {
+        self.page = page
+        self.size = size
+    }
+}
+
+public struct PageSummary: Codable {
+    public let begin: Int
+    public let end: Int
+    public let total: Int
+
+    public init(begin: Int, end: Int, total: Int) {
+        self.begin = begin
+        self.end = end
+        self.total = total
+    }
+}
+
+public struct PageContent<T: Codable>: Codable {
+    public let contents: [T]
+    public let index: PageIndex
+    public let items: PageSummary
+    public let pages: PageSummary
+
+    public init(contents: [T], index: PageIndex, items: PageSummary, pages: PageSummary) {
+        self.contents = contents
+        self.index = index
+        self.items = items
+        self.pages = pages
+    }
+}
